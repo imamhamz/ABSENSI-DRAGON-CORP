51ed6f3fe8d20ffe42f8d7e9845c17354a3ee170
imam hamjah <135954018+imamhamz@users.noreply.github.com>
2026-08-27 09:06:39 +0700

    Delete dragon-corp-dashboard-connected directory

--- dragon-corp-dashboard-connected/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-const SUPABASE_URL = "https://ihwqgxxwbrbbqqhjmoxz.supabase.co";
-const SUPABASE_KEY = "sb_publishable_RgEkyhJtoz0QWB10oUwA_g_Ngxpdp1q";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
