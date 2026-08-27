6b7acaf9d4ef32d215e23b742c85ebe07faa2fd7
imam hamjah <135954018+imamhamz@users.noreply.github.com>
2026-08-27 09:07:50 +0700

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 const SUPABASE_URL = "https://ihwqgxxwbrbbqqhjmoxz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_RgEkyhJtoz0QWB10oUwA_g_Ngxpdp1q";
 
-const SUPABASE_KEY =
-  "sb_publishable_RgEkyhJtoz0QWB10oUwA_g_Ngxpdp1q";
-
-export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
