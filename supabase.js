741a96aa89856a330d28ddf81723d3957f682947
imam hamjah <135954018+imamhamz@users.noreply.github.com>
2026-08-27 17:07:44 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-export const supabase = createClient(
-  "https://ihwqgxxwbrbbqqhjmoxz.supabase.co",
-  "sb_publishable_RgEkyhJtoz0QWB10oUwA_g_Ngxpdp1q"
-);
-export const EMPLOYEE_TABLE = "absensi-dragon-corp";
-export const ATTENDANCE_TABLE = "attendance";
-export const PHOTO_BUCKET = "attendance-photos";
